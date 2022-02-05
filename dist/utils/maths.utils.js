61f76d81344641420244b71530636c51ff37fcb4
Sabalot <s.sabalot@gmail.com>
2022-02-05 10:53:43 +0100

    remove dist

--- dist/utils/maths.utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addition = void 0;
-const addition = (a, b) => {
-    return a + b;
-};
-exports.addition = addition;
-//# sourceMappingURL=maths.utils.js.map
